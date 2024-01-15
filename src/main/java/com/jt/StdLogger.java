8b95e61e5779d88b899becf4ead90f9f14fbecc1
resin8or <jamie.tobin@gmail.com>
2024-01-15 12:25:38 +0000

    Code added from copilot changes

--- src/main/java/com/jt/StdLogger.java
+++ src/main/java/com/jt/StdLogger.java
@@ -2,6 +2,8 @@ package com.jt;
 
 import org.apache.log4j.Logger;
 
+import java.util.Date;
+
 public class StdLogger {
 
 	final static Logger logger = Logger.getLogger(StdLogger.class);
@@ -10,7 +12,25 @@ public class StdLogger {
 		logger.info ("test");
 		logger.debug("debug test");
 		logger.error("error test");
-		
+		// call the calculateDaysBetweenDates method
+		// create a variable to hold a date value and set the date to yesterday's date
+		Date yesterday = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000);
+		// create a variable to hold a date value and set the date to today's date
+		Date today = new Date();
+		Test test = new Test();
+		//create a variable to hold the number of days between yesterday and today
+		int daysBetween = test.calculateDaysBetweenDates(today, yesterday);
+		// output the result of the test.calculateDaysBetweenDates method
+		System.out.println("The number of days between " + yesterday + " and " + today + " is " + daysBetween);
+		// call the nearestLeapYear method
+		// create a variable to hold a date value and set the date to today's date
+		Date date = new Date();
+		// create a variable to hold the nearest leap year
+		int nearestLeapYear = test.nearestLeapYear(today);
+		// output the result of the test.nearestLeapYear method
+		System.out.println("The nearest leap year to " + date + " is " + nearestLeapYear);
+
 	}
+	
 	
 }
--- src/main/java/com/jt/Test.java
+++ src/main/java/com/jt/Test.java
@@ -1,2 +1,48 @@
-package com.jt;public class Test {
+package com.jt;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+/**
+ * This class is used to calculate the number of days between two dates.
+ *
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class Test {
+    Logger logger = Logger.getLogger(Test.class.getName());
+
+    public int calculateDaysBetweenDates(Date date1, Date date2) {
+        return (int) ((date2.getTime() - date1.getTime()) / (1000 * 60 * 60 * 24));
+
+    }
+
+    /// create a new method that returns the nearest leap year for a date
+    // create a new method that returns the nearest leap year for a date
+	public int nearestLeapYear(Date date) {
+		// create a variable to hold the year value of the date
+		// The method getYear() from the type Date is deprecated
+		// find the year from a date value
+		@SuppressWarnings("deprecation")
+		int year = date.getYear();
+        
+        // the year value is not correct, so add 1900 to it
+        year = year + 1900;
+        logger.info(year + " is the year");
+
+		// create a variable to hold the nearest leap year
+		int nearestLeapYear = 0;
+		// if the year is divisible by 4, then it is a leap year
+		if (year % 4 == 0) {
+			nearestLeapYear = year;
+		} else {
+			// otherwise, find the nearest leap year
+			nearestLeapYear = year + (4 - (year % 4));
+		}
+		// return the nearest leap year
+		return nearestLeapYear;
+	}   
+
+
 }
